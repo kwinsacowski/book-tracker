e5ad53eeb16ede7b319c0cf14e1e7f0c25806395
Kayla Salmon <kayla.wienczkowski@yahoo.com>
2026-02-28 15:17:14 -0500

    Fix prisma relations and genre uniqueness

---
+++ backend/prisma/migrations/20260228201656_fix_book_genres_and_genre_unique/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[name]` on the table `Genre` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE INDEX "Book_isbn_idx" ON "Book"("isbn");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Genre_name_key" ON "Genre"("name");
